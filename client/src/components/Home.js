5ba33162eaf61b4a5bed0ed73b46e461a2a08a88
Bukkysos <chukwuebukaanyanwusos@gmail.com>
2018-09-25 17:30:37 +0100

    The parent home component for home header and banner

---
+++ client/src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import Header from './Header';
+import HomeBanner from './HomeBanner';
+
+class Home extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+                <Header/>
+                <HomeBanner/>
+            </div>
+        )
+    }
+}
+
+export default Home;
